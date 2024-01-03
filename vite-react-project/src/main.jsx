8608f3d0cddb3dfedcee6d21a178ff9955980550
roman <51534566+roman-thapa@users.noreply.github.com>
2024-01-03 08:21:43 +0530

    Use react create element

--- vite-react-project/src/main.jsx
+++ vite-react-project/src/main.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const aReactVariable = React.createElement(
+    'a',
+    {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    "Visit GOOGLE"
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
+    <>
+        <App />
+        {aReactVariable}
+    </>
 )
