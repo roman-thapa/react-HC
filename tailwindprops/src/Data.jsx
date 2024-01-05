04281d0134188a1452b4d3f489ab1e8d8769d0c3
roman <51534566+roman-thapa@users.noreply.github.com>
2024-01-05 09:14:12 +0530

    Make another folder and use tailwind and props

---
+++ tailwindprops/src/Data.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Data() {
+  return (
+    [
+      {
+        name: "Eldoria Haven",
+        location: "Mystic Isles",
+        description: "Eldoria Haven, nestled in the heart of the Mystic Isles, is a serene sanctuary surrounded by enchanted forests. The air is filled with whispers of ancient magic, and vibrant flora paints a surreal landscape, making it a haven for seekers of mystical wonders.",
+        imgSrc: "https://res.cloudinary.com/jerrick/image/upload/d_642250b563292b35f27461a7.png,f_jpg,fl_progressive,q_auto,w_1024/64bb9df6bf5196001d26898f.jpg"
+      },
+      {
+        name: "Celestial Citadel",
+        location: "Astral Peaks",
+        description: "Perched atop the Astral Peaks, Celestial Citadel commands breathtaking views of cosmic phenomena. Its crystalline spires reflect the hues of distant galaxies, and the ethereal ambiance invites visitors to contemplate the vast mysteries of the universe in this celestial retreat.",
+        imgSrc: "https://img.freepik.com/premium-photo/celestial-citadel-ai_972075-9460.jpg"
+      },
+      {
+        name: "Whispering Oasis",
+        location: "Silent Dunes",
+        description: "Nestled within the Silent Dunes, the Whispering Oasis is a tranquil refuge where golden sands stretch endlessly. The gentle breeze carries whispers of ancient tales, and the oasis, adorned with emerald palms, offers a serene haven for those seeking respite in the arid expanse.",
+        imgSrc: "https://ik.imagekit.io/storybird/images/9084a0f2-aa57-46cb-8f18-83fd30a212d0/0_654744580.png"
+      },
+      {
+        name: "Luminara Lagoon",
+        location: "Iridescent Isles",
+        description: "Luminara Lagoon, situated amidst the Iridescent Isles, is a captivating aquatic wonder. The waters shimmer with iridescent hues, and rare bioluminescent creatures illuminate the night. Visitors are enchanted by the lagoon's ethereal beauty, creating a surreal experience beneath the starlit sky.",
+        imgSrc : "https://pics.craiyon.com/2023-06-27/b8b67cedb29d4ddeac17ab8bcdf8601b.webp"
+      },
+      {
+        name: "Verdant Valley",
+        location: "Sylvan Groves",
+        description: "Nestled within the Sylvan Groves, Verdant Valley is a lush, emerald paradise where ancient trees tower majestically. Sunbeams filter through the dense foliage, creating a tapestry of light and shadow. The valley's vibrant flora and harmonious bird songs make it an idyllic retreat for nature enthusiasts.",
+        imgSrc: "https://play.idlescape.com/images/foraging/verdant_valley.png"
+      }
+    ]
+  )
+}
+
+export default Data
