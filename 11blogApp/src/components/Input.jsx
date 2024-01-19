5d52520c907883119c33a5182a45b7cc851efec2
roman <51534566+roman-thapa@users.noreply.github.com>
2024-01-19 10:15:06 +0530

    Make basic components

---
+++ 11blogApp/src/components/Input.jsx
@@ -0,0 +1,30 @@
+import React, { forwardRef, useId } from 'react'
+
+const Input = forwardRef(function Input({
+    lable,
+    type = 'text', 
+    className = '', 
+    ...props},
+    ref) {
+        const id = useId()
+
+        return (
+            <div className='w-full'>
+                {lable && (
+                    <lable htmlFor={id}
+                    className='inline-block mb-1 pl-1'>
+                        {lable}
+                    </lable>
+                )}
+                <input 
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                type={type}
+                ref = {ref}
+                {...props}
+                id={id}
+                />
+            </div>
+        )
+})
+
+export default Input
